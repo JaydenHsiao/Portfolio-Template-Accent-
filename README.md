3e8701f50114ae011a04a7d9760e1159ae7db5d0
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2019-09-23 10:33:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio-Template--Accent-
+ Portfolio Website
---
+++ js/project.js
@@ -0,0 +1,95 @@
+  function openNavMGSF() {
+      document.getElementById("myNavMGSF").style.width = "100%";
+  }
+
+  function closeNavMGSF() {
+      document.getElementById("myNavMGSF").style.width = "0%";
+  }
+
+  function openNavYAR() {
+      document.getElementById("myNavYAR").style.width = "100%";
+  }
+
+  function closeNavYAR() {
+      document.getElementById("myNavYAR").style.width = "0%";
+  }
+
+  function openNavCCF() {
+      document.getElementById("myNavCCF").style.width = "100%";
+  }
+
+  function closeNavCCF() {
+      document.getElementById("myNavCCF").style.width = "0%";
+  }
+
+  function openNavYB() {
+      document.getElementById("myNavYB").style.width = "100%";
+  }
+
+  function closeNavYB() {
+      document.getElementById("myNavYB").style.width = "0%";
+  }
+
+  function openNavDUI() {
+      document.getElementById("myNavDUI").style.width = "100%";
+  }
+
+  function closeNavDUI() {
+      document.getElementById("myNavDUI").style.width = "0%";
+  }
+
+  function openNavTP() {
+      document.getElementById("myNavTP").style.width = "100%";
+  }
+
+  function closeNavTP() {
+      document.getElementById("myNavTP").style.width = "0%";
+  }
+
+  function openNavHDSW() {
+      document.getElementById("myNavHDSW").style.width = "100%";
+  }
+
+  function closeNavHDSW() {
+      document.getElementById("myNavHDSW").style.width = "0%";
+  }
+
+  function openNavHMD() {
+      document.getElementById("myNavHMD").style.width = "100%";
+  }
+
+  function closeNavHMD() {
+      document.getElementById("myNavHMD").style.width = "0%";
+  }
+
+  function openNavMLH() {
+      document.getElementById("myNavMLH").style.width = "100%";
+  }
+
+  function closeNavMLH() {
+      document.getElementById("myNavMLH").style.width = "0%";
+  }
+
+  function openNavJPC() {
+      document.getElementById("myNavJPC").style.width = "100%";
+  }
+
+  function closeNavJPC() {
+      document.getElementById("myNavJPC").style.width = "0%";
+  }
+
+  function openNavHEROS() {
+      document.getElementById("myNavHEROS").style.width = "100%";
+  }
+
+  function closeNavHEROS() {
+      document.getElementById("myNavHEROS").style.width = "0%";
+  }
+
+  function openNavBFAM() {
+      document.getElementById("myNavBFAM").style.width = "100%";
+  }
+
+  function closeNavBFAM() {
+      document.getElementById("myNavBFAM").style.width = "0%";
+  }
